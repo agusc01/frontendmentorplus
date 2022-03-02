de290725cf6370ef9c90f073d8a7d0499c1df903
Agustin <33077305+agusc01@users.noreply.github.com>
2022-03-02 17:35:37 -0300

    Almost finished 07-faq-accordion-card-main

---
+++ projects/07-faq-accordion-card-main/dist/js/app.js
@@ -0,0 +1,55 @@
+const accordions = document.querySelectorAll('.accordion');
+const accordionTitles = document.querySelectorAll(
+  '.accordion__title'
+);
+
+const selectCard = (target) => {
+  // the goal is to take accordion
+  let accordion = target.parentElement;
+  if (target.classList.contains('accordion__arrow')) {
+    accordion = accordion.parentElement;
+  }
+  return accordion;
+};
+
+const toggleArrow = (accordion) => {
+  let cardArrow = accordion.querySelector('.accordion__arrow');
+  cardArrow.classList.toggle('accordion__arrow--open');
+};
+
+const toggleText = (accordion) => {
+  let accordionContainerText = accordion.querySelector(
+    '.accordion__container'
+  );
+  accordionContainerText.classList.toggle(
+    'accordion__container--visible'
+  );
+};
+
+const toggleTitle = (accordion) => {
+  let accordionTitle = accordion.querySelector('.accordion__title');
+  accordionTitle.classList.toggle('accordion__title--bolder');
+};
+
+const toggleSection = (accordion) => {
+  toggleArrow(accordion);
+  toggleText(accordion);
+  toggleTitle(accordion);
+};
+
+accordionTitles.forEach((accordionTitle) => {
+  accordionTitle.addEventListener(
+    'click',
+    (event) => {
+      let accordion = selectCard(event.target);
+      toggleSection(accordion);
+    },
+    false
+  );
+});
+
+accordions.forEach((accordion) => {
+  if (accordion.dataset.closed === 'false') {
+    toggleSection(accordion);
+  }
+});
