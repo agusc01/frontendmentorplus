a89bec4f26b1419527dfe0c347d25a342ba197ee
Agustin <33077305+agusc01@users.noreply.github.com>
2022-03-04 23:51:07 -0300

    Working on 09-article-... desktop active state

--- projects/09-article-preview-component-master/dist/js/app.js
+++ projects/09-article-preview-component-master/dist/js/app.js
@@ -2,6 +2,16 @@ const shareButton = document.querySelector('.card__icon--share');
 const shareSection = document.querySelector('#card__share-section');
 const links = document.querySelector('.card__share-section--links');
 shareButton.addEventListener('click', () => {
+  if (window.innerWidth < 921) {
+    shareSection.classList.toggle('opacity-0');
+  }
   links.classList.toggle('opacity-1');
-  shareSection.classList.toggle('opacity-0');
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 920) {
+    shareSection.classList.remove('opacity-0');
+  } else if (links.classList.contains('opacity-1')) {
+    shareSection.classList.add('opacity-0');
+  }
 });
