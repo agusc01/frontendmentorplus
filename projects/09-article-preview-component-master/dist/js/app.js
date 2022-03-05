c3fbd88f9c7f8d36d7ca4cfed06b200090a81794
Agustin <33077305+agusc01@users.noreply.github.com>
2022-03-05 00:24:57 -0300

    Still working on desktop active state...

--- projects/09-article-preview-component-master/dist/js/app.js
+++ projects/09-article-preview-component-master/dist/js/app.js
@@ -1,7 +1,26 @@
 const shareButton = document.querySelector('.card__icon--share');
+const facebook = document.querySelector('.card__icon--facebook');
 const shareSection = document.querySelector('#card__share-section');
 const links = document.querySelector('.card__share-section--links');
+const icons = document.querySelectorAll('.card__icon');
+
 shareButton.addEventListener('click', () => {
+  // This it's because .card__share-section has opacity instead display
+  // =============== start ===============
+  let classNameToRemove = 'd-none';
+  let classNameToAdd = 'd-block';
+  if (links.classList.contains('opacity-1')) {
+    classNameToRemove = 'd-block';
+    classNameToAdd = 'd-none';
+  }
+  icons.forEach((icon) => {
+    if (!icon.classList.contains('card__icon--share')) {
+      icon.classList.add(`${classNameToAdd}`);
+      icon.classList.remove(`${classNameToRemove}`);
+    }
+  });
+  // =============== end ===============
+
   if (window.innerWidth < 921) {
     shareSection.classList.toggle('opacity-0');
   }
