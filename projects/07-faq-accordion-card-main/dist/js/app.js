b14f0fd2b5f17bdf5ed17939f9863f2dc5967d94
Agustin <33077305+agusc01@users.noreply.github.com>
2022-03-13 15:08:16 -0300

    Awesome changed in 07-faq-accordion-card-main

--- projects/07-faq-accordion-card-main/dist/js/app.js
+++ projects/07-faq-accordion-card-main/dist/js/app.js
@@ -1,61 +1,83 @@
+// ========================= NOW =============================
 const accordions = document.querySelectorAll('.accordion');
-const titles = document.querySelectorAll('.accordion__title');
+const headers = document.querySelectorAll('.accordion__header');
 
-const selectCard = (target) => {
-  // the goal is to take accordion
-  let accordion = target.parentElement;
-  if (target.classList.contains('accordion__arrow')) {
-    accordion = accordion.parentElement;
-  }
-  return accordion;
-};
-
-const toggleArrow = (accordion) => {
-  let arrow = accordion.querySelector('.accordion__arrow');
-  arrow.classList.toggle('accordion__arrow--open');
-};
-
-const toggleText = (accordion) => {
-  let text = accordion.querySelector('.accordion__container');
-  text.classList.toggle('accordion__container--visible');
-};
-
-const toggleTitle = (accordion) => {
-  let title = accordion.querySelector('.accordion__title');
-  title.classList.toggle('accordion__title--bolder');
-};
-
-const closeAllAccordions = () => {
+const closeAllItems = () => {
   accordions.forEach((accordion) => {
-    let title = accordion.querySelector('.accordion__title');
-    title.classList.remove('accordion__title--bolder');
-    let arrow = accordion.querySelector('.accordion__arrow');
-    arrow.classList.remove('accordion__arrow--open');
-    let text = accordion.querySelector('.accordion__container');
-    text.classList.remove('accordion__container--visible');
+    accordion.classList.remove('accordion--active');
   });
 };
 
-const toggleSection = (accordion) => {
-  closeAllAccordions();
-  toggleArrow(accordion);
-  toggleText(accordion);
-  toggleTitle(accordion);
-};
-
-titles.forEach((title) => {
-  title.addEventListener(
-    'click',
-    (event) => {
-      let accordion = selectCard(event.target);
-      toggleSection(accordion);
-    },
-    false
-  );
+headers.forEach((header) => {
+  header.addEventListener('click', (e) => {
+    console.log(e.target);
+    let accordion = header.parentElement;
+    let wasItOpen = accordion.classList.contains('accordion--active');
+    closeAllItems();
+    if (wasItOpen === false) {
+      accordion.classList.add('accordion--active');
+    }
+  });
 });
 
-accordions.forEach((accordion) => {
-  if (accordion.dataset.closed === 'false') {
-    toggleSection(accordion);
-  }
-});
+// ======================= BEFORE ========================
+// const titles = document.querySelectorAll('.accordion__title');
+
+// const selectCard = (target) => {
+//   // the goal is to take accordion
+//   let accordion = target.parentElement;
+//   if (target.classList.contains('accordion__arrow')) {
+//     accordion = accordion.parentElement;
+//   }
+//   return accordion;
+// };
+
+// const toggleArrow = (accordion) => {
+//   let arrow = accordion.querySelector('.accordion__arrow');
+//   arrow.classList.toggle('accordion__arrow--open');
+// };
+
+// const toggleText = (accordion) => {
+//   let text = accordion.querySelector('.accordion__body');
+//   text.classList.toggle('accordion__body--visible');
+// };
+
+// const toggleTitle = (accordion) => {
+//   let title = accordion.querySelector('.accordion__title');
+//   title.classList.toggle('accordion__title--bolder');
+// };
+
+// const closeAllAccordions = () => {
+//   accordions.forEach((accordion) => {
+//     let title = accordion.querySelector('.accordion__title');
+//     title.classList.remove('accordion__title--bolder');
+//     let arrow = accordion.querySelector('.accordion__arrow');
+//     arrow.classList.remove('accordion__arrow--open');
+//     let text = accordion.querySelector('.accordion__body');
+//     text.classList.remove('accordion__body--visible');
+//   });
+// };
+
+// const toggleSection = (accordion) => {
+//   closeAllAccordions();
+//   toggleArrow(accordion);
+//   toggleText(accordion);
+//   toggleTitle(accordion);
+// };
+
+// titles.forEach((title) => {
+//   title.addEventListener(
+//     'click',
+//     (event) => {
+//       let accordion = selectCard(event.target);
+//       toggleSection(accordion);
+//     },
+//     false
+//   );
+// });
+
+// accordions.forEach((accordion) => {
+//   if (accordion.dataset.closed === 'false') {
+//     toggleSection(accordion);
+//   }
+// });
