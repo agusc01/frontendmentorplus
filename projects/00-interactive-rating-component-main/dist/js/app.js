6e4d36f6daf0ce8f2e04e8ed22eb24f539716085
Agustin <33077305+agusc01@users.noreply.github.com>
2022-03-20 01:44:59 -0300

    Finished mobile version 00-interactive... with .js

---
+++ projects/00-interactive-rating-component-main/dist/js/app.js
@@ -0,0 +1,41 @@
+const form = document.querySelector('.form');
+const submit = document.querySelector('#submit');
+const card = document.querySelector('.card');
+const feedBack = document.querySelector('.card__feedback');
+const radioInputs = document.querySelectorAll('.rated__input');
+
+const getValueRadioInput = () => {
+  // You can't put a "return" inside of a "forEach"
+  // Doesn't work... So I write it with a simple "for"
+  // You can do it , as I do. The problem is if you do it
+  // without return, if there're 10000 inputs and the
+  // first one is checked you gonna make it 10000 times
+  // It just wasting time and PC resources
+
+  // let value = -1;
+  // radioInputs.forEach((input) => {
+  //   // console.log(input.checked, input.value);
+  //   if (input.checked) {
+  //     value = input.value;
+  //   }
+  // });
+  // return value;
+
+  for (let i = 0; i < radioInputs.length; i++) {
+    if (radioInputs[i].checked) {
+      return radioInputs[i].value;
+    }
+  }
+  return -1;
+};
+
+form.addEventListener('click', (e) => {
+  if (e.target == submit) {
+    e.preventDefault();
+
+    card.classList.add('sent');
+    feedBack.textContent = `You selected ${getValueRadioInput()} out of ${
+      radioInputs.length
+    }`;
+  }
+});
